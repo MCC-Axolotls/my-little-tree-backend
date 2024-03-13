a67836b889dd6acea3ea58e1e3be659236ef4f1f
alfredopizana <pizana.alfredo@gmail.com>
2024-03-13 00:48:19 -0600

    Created schema for plants and posts

---
+++ src/models/plants.model.js
@@ -0,0 +1,59 @@
+import mongoose, { Schema } from "mongoose";
+
+const plantSchema = new mongoose.Schema({
+    nickname: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 200,
+        trim: true
+    },
+    plantType: {
+        type: String
+    },
+    description: {
+        type: String,
+    },
+    wateringFrequency: {
+        type: Number,
+    },
+    lastWatered: {
+        type: Date,
+    },
+    nextWatering: {
+        type: Date,
+    },
+    dismissedWatering:{
+        type: Boolean
+    },
+
+    fertilizerFrequency: {
+        type: Number,
+    },
+    lastFertitlizer: {
+        type: Date,
+    },
+    nextFertilizer: {
+        type: Date,
+    },
+    dismissedFertilizer:{
+        type: Boolean
+    },
+    imageUrl:{
+        type: String
+    },
+    userId: {
+        type: [
+          {
+            type: Schema.Types.ObjectId,
+            ref: "Users",
+            required: true,
+          },
+        ],
+    },
+})
+
+const Plant = mongoose.model('plants', plantSchema)
+
+
+export { Plant } 
