76b6c6a22da61472fd107517d893dddb93d664e5
alfredopizana <pizana.alfredo@gmail.com>
2024-03-12 23:56:51 -0600

    Configured DB and tested connection (#2)

--- src/libs/db.js
+++ src/libs/db.js
@@ -8,8 +8,7 @@ const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST } = process.env
 const URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`
 
 const connect = () => {
-    //return mongoose.connect(URL)
-    return new Promise((resolve,reject)=>{resolve()})
+    return mongoose.connect(URL)
 }
 
 export default connect
